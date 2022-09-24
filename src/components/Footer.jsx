eeb4fb615c7fd5638d9088dfc396b4ad2547370a
Dundgren <danne.l.97@hotmail.com>
2022-09-24 11:53:32 +0200

    Added names to footer links. Added margin to mobile title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <footer>
-            <a href="https://www.linkedin.com/in/daniel-lundgren-007b871b3/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/daniel-lundgren-007b871b3/" target="_blank" rel="noreferrer" aria-label="Link to Daniels Linkedin">
                 <i className="fa-brands fa-linkedin"></i>
             </a>
-            <a href="https://github.com/Dundgren" target="_blank" rel="noreferrer">
+            <a href="https://github.com/Dundgren" target="_blank" rel="noreferrer" aria-label="Link to Daniels Github">
                 <i className="fa-brands fa-square-github"></i>
             </a>
         </footer>
