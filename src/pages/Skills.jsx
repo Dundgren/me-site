b375e30d27eb845c639583357a1280e4b88616fc
Dundgren <danne.l.97@hotmail.com>
2022-10-04 17:22:34 +0200

    Added icons and some placeholder text. Added styling for skillspage and moved screenshot styling to skills.scss

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,75 +2,34 @@ import chatboxImage from "../assets/img/chatbox.png";
 
 export default function Skills() {
     return (
-        <>
-            <h1>My Skills</h1>
-
-            <p>My resumé is acting as a placeholder for this page. Update coming soon!</p>
-            <div>
-                <h2>Education</h2>
-                <ul>
-                    <li>201308 - 201606 <b>Östra Gymnasiet</b>, Skogås, Technology program.</li>
-                    <ul>
-                        <li>
-                            Information and media technology as focus with courses such as Programming, Entrepreneurship, Web development and Math 1c, 2c ,3c.
-                        </li>
-                    </ul>
-                    <li>202008 - 202206 <b>Blekinge Tekniska Högskola</b>, Higher education in Web programming 120hp.</li>
-                    <ul>
-                        <li>
-                            First year courses were: Web Technologies (HTML and CSS), Python, JavaScript, Object-oriented Python, Design, Database and Model View Controller/Object-oriented PHP.
-                        </li>
-                        <li>
-                            Second year courses were: Virtual environments (Linux, Docker and Virtualbox), Group Project Course, Computer and tele-communication, JavaScript-based frameworks, Mathematical Modelling, Basic System Verification and Examination project.
-                        </li>
-                    </ul>
-                </ul>
-                <h2>Experience</h2>
-                <ul>
-                    <li>201708 - 201802 <b>Södersjukhuset</b>, Stockholm, Transporter, Hourly.</li>
-                    <ul>
-                        <li>
-                            Main task was to transport patients between the hospital's departments.
-                        </li>
-                        <li>
-                            Misc. tasks included cleaning stretchers, waste management and transporting deceased patients to the morgue.
-                        </li>
-                    </ul>
-                    <li>201802 - 201909 <b>Bauhaus</b>, Länna, Warehouse worker, Temporary employment.</li>
-                    <ul>
-                        <li>
-                            Main task was receiving goods from suppliers using a scanner.
-                        </li>
-                        <li>
-                            Misc. tasks included among others: Operation of forklift and skylift in the store, unloading trucks, order picking, customer service and stocking goods.
-                        </li>
-                    </ul>
-                </ul>
-                <h2>Languages</h2>
-                <ul>
-                    <li>JavasScript/Node/Vue/React</li>
-                    <li>PHP</li>
-                    <li>Python</li>
-                    <li>HTML</li>
-                    <li>CSS/SCSS</li>
-                    <li>SQL</li>
-                    <li>TypeScript</li>
-                </ul>
-                <h2>Tools</h2>
-                <ul>
-                    <li>Docker</li>
-                    <li>Git</li>
-                    <li>GraphQL</li>
-                    <li>SQLite</li>
-                    <li>MySQL</li>
-                </ul>
-            </div>
+        <div className="skills">
+            <h1>My skills</h1>
+            <i class="fa-brands fa-square-js"></i>
+            <i class="fa-brands fa-php"></i>
+            <i class="fa-brands fa-python"></i>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <i class="fa-brands fa-vuejs"></i>
+            <i class="fa-brands fa-react"></i>
+            <i class="fa-brands fa-html5"></i>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <i class="fa-brands fa-css3-alt"></i>
+            <i class="fa-brands fa-sass"></i>
+            <i class="fa-brands fa-docker"></i>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-solid fa-wrench"></i>
             <figure className="screenshot">
                 <a href="https://chatbox-react-tan.vercel.app" target="_blank" rel="noreferrer" aria-label="Link to ChatBox">
                     <img src={chatboxImage} alt="Screenshot of Daniel Lundgrens app ChatBox." />
                 </a>
                 <figcaption>Please check out my little web-app ChatBox! It was originally created in Vue and later recreated using React. Both versions can be found on my github!</figcaption>
             </figure>
-        </>
+        </div>
     )
 }
